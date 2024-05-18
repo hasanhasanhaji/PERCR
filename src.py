680ba889483148418b72c3e2a0b7c7c81e6279bd
sorenagorgan <hasanhasanhaji@gmail.com>
2024-05-18 10:02:26 +0330

    update src
    finish char cnn

--- src.py
+++ src.py
@@ -3,10 +3,62 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from conll_mehr import *
+from utils import *
 
 # configure logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+
+class CharCNN(nn.Module):
+    """ Character-level CNN. Contains character embeddings.
+    """
+
+    unk_idx = 1
+    vocab = read_corpus('data/Mehr/train-dev').char_vocab
+    _stoi = {char: idx + 2 for idx, char in enumerate(vocab)}
+    pad_size = 15
+
+    def __init__(self, filters, char_dim=8):
+        super().__init__()
+
+        self.embeddings = nn.Embedding(len(self.vocab) + 2, char_dim, padding_idx=0)
+        self.convs = nn.ModuleList([nn.Conv1d(in_channels=self.pad_size,
+                                              out_channels=filters,
+                                              kernel_size=n) for n in (3, 4, 5)])
+
+    def forward(self, sent):
+        """ Compute filter-dimensional character-level features for each doc token """
+        embedded = self.embeddings(self.sent_to_tensor(sent))
+        convolved = torch.cat([F.relu(conv(embedded)) for conv in self.convs], dim=2)
+        pooled = F.max_pool1d(convolved, convolved.shape[2]).squeeze(2)
+        return pooled
+
+    def sent_to_tensor(self, sent):
+        """ Batch-ify a document class instance for CharCNN embeddings """
+        tokens = [self.token_to_idx(t) for t in sent]
+        batch = self.char_pad_and_stack(tokens)
+        return batch
+
+    def token_to_idx(self, token):
+        """ Convert a token to its character lookup ids """
+        return to_cuda(torch.tensor([self.stoi(c) for c in token]))
+
+    def char_pad_and_stack(self, tokens):
+        """ Pad and stack an uneven tensor of token lookup ids """
+        skimmed = [t[:self.pad_size] for t in tokens]
+
+        lens = [len(t) for t in skimmed]
+
+        padded = [F.pad(t, (0, self.pad_size - length))
+                  for t, length in zip(skimmed, lens)]
+
+        return torch.stack(padded)
+
+    def stoi(self, char):
+        """ Lookup char id. <PAD> is 0, <UNK> is 1. """
+        idx = self._stoi.get(char)
+        return idx if idx else self.unk_idx
 
 
 class DocumentEncoder(nn.Module):
@@ -19,7 +71,15 @@ class DocumentEncoder(nn.Module):
 
         #  Unit vector embeddings >>> normalization
         logger.info("Start normalizing glove weights.")
-        glove_weights = F.normalize(GLOVE.weights())
+        glove_weights = F.normalize(GLOVE.weights())  # unique vocabs ** 300 (glove dim)
+
+        # GLoVE
+        self.glove = nn.Embedding(glove_weights.shape[0], glove_weights.shape[1])
+        self.glove.weight.data.copy_(glove_weights)
+        self.glove.weight.requires_grad = False
+
+        # Character
+        self.char_embeddings = CharCNN(char_filters)  # Create char nn layer
         pass
 
 
--- utils.py
+++ utils.py
@@ -1,3 +1,13 @@
+import torch
+
+
 def flatten(alist):
     """ Flatten a list of lists into one list """
     return [item for sublist in alist for item in sublist]
+
+
+def to_cuda(x):
+    """ GPU-enable a tensor """
+    if torch.cuda.is_available():
+        x = x.cuda()
+    return x
